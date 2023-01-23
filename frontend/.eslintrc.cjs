e577340b45353840b91401c3f9a6a2d4cab1f3ad
Rafael Mendes <mendesrafa.c@gmail.com>
2023-01-23 00:35:22 -0300

    ADD: config files

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -22,5 +22,7 @@ module.exports = {
   plugins: ['react', '@typescript-eslint', 'prettier'],
   rules: {
     'react/react-in-jsx-scope': 0,
+    '@typescript-eslint/no-non-null-assertion': 0,
+    'react-hooks/exhaustive-deps': 0,
   },
 };
