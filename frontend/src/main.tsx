c55e5541045ea2111b59c04721a4773275cd48e4
Rafael Mendes <mendesrafa.c@gmail.com>
2023-01-23 00:50:07 -0300

    UPD: app and main components

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './Components/App';
+import { BrowserRouter } from 'react-router-dom';
+import AuthProvider from './context/AuthProvider';
+
+import App from './App';
 import './styles/main.css';
+import AppointmentProvider from './context/AppointmentProvider';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <AppointmentProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AppointmentProvider>
+    </AuthProvider>
   </React.StrictMode>,
 );
