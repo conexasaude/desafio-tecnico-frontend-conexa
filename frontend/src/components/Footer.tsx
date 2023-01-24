ae636c9571476bf11a967562677bf5f78d2a530e
Lucas <lucas@Lucass-MacBook-Pro.local>
2023-01-24 11:43:27 -0300

    update: footer styles

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,6 +1,6 @@
 export function Footer() {
   return (
-    <div className="flex border-b-2 py-10 w-full">
+    <div className="flex py-5 w-full border-t-2 lg:border-gray-300">
       <button className="default w-20 mr-auto">Ajuda</button>
       <button className="filled w-40">Agendar consulta</button>
     </div>
