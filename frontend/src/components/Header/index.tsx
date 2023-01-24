417003d4eb4bb9c94bf7ae8363e1381c6f45d40c
Lucas <lucas@Lucass-MacBook-Pro.local>
2023-01-24 11:27:59 -0300

    update: header styles

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -7,11 +7,7 @@ interface HeaderProps {
 function Header({ session }: HeaderProps) {
   return (
     <header className="h-20 drop-shadow-lg bg-white">
-      <Container
-        className={`flex items-center h-full p-5 ${
-          !session ? "justify-center" : "justify-between"
-        }`}
-      >
+      <Container className="flex items-center h-full p-5 justify-center lg:justify-start mr-auto">
         <img src="/logo.png" alt="logo" className="h-10" />
         {session && <UserArea name="User" />}
       </Container>
