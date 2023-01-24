16420fb23ea2b26c9c65e103f3268ece14ec24d4
Lucas <lucas@Lucass-MacBook-Pro.local>
2023-01-24 11:57:51 -0300

    update: appointments area styles

--- frontend/src/components/Appointments/index.tsx
+++ frontend/src/components/Appointments/index.tsx
@@ -5,7 +5,8 @@ export function Appointments({ appointments }: { appointments: AppointmentProps[
   return (
     <div className="flex flex-col space-y-5 w-full items-center">
       <p className="text-lg text-gray-800">{appointments.length} consultas agendadas</p>
-      <div className="w-full max-w-[600px] space-y-10">
+
+      <div className="w-full max-w-[600px] space-y-10 overflow-y-auto max-h-[400px]">
         {appointments.map((appointment) => (
           <AppointmentCard appointment={appointment} />
         ))}
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -50,6 +50,50 @@ export function Dashboard() {
         email: "saruman.thewhite@mail.com",
       },
     },
+    {
+      id: 1,
+      patientId: 1,
+      date: "Fri Feb 05 2021 10:20:00 GMT-0300 (Brasilia Standard Time)",
+      patient: {
+        id: 1,
+        first_name: "Frodo",
+        last_name: "Baggins",
+        email: "frodo.baggins@mail.com",
+      },
+    },
+    {
+      id: 2,
+      patientId: 3,
+      date: "Thu Feb 11 2021 09:00:00 GMT-0300 (Brasilia Standard Time)",
+      patient: {
+        id: 3,
+        first_name: "Saruman",
+        last_name: "The White",
+        email: "saruman.thewhite@mail.com",
+      },
+    },
+    {
+      id: 3,
+      patientId: 2,
+      date: "Thu Feb 11 2021 10:00:00 GMT-0300 (Brasilia Standard Time)",
+      patient: {
+        id: 2,
+        first_name: "Samwise",
+        last_name: "Gamgee",
+        email: "samwise.gamgee@mail.com",
+      },
+    },
+    {
+      id: 4,
+      patientId: 3,
+      date: "Thu Feb 11 2021 13:00:00 GMT-0300 (Brasilia Standard Time)",
+      patient: {
+        id: 3,
+        first_name: "Saruman",
+        last_name: "The White",
+        email: "saruman.thewhite@mail.com",
+      },
+    },
   ];
 
   return (
