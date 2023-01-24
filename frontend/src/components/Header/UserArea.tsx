edd0e4cbb98b517cff48de8b1ce957cd6afb3dfa
Lucas <lucas@Lucass-MacBook-Pro.local>
2023-01-24 10:51:00 -0300

    fix: logout button size

--- frontend/src/components/Header/UserArea.tsx
+++ frontend/src/components/Header/UserArea.tsx
@@ -7,7 +7,7 @@ export function UserArea({ name }: UserAreaProps) {
     <div className="flex gap-2 items-center w-full text-end justify-end">
       <p className="w-full invisible md:visible">Olá, {name}</p>
       <div className="w-20">
-        <button className="default">Sair</button>
+        <button className="default w-20">Sair</button>
       </div>
     </div>
   );
