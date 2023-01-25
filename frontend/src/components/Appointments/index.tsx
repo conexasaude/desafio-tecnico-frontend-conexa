ec498f3d5f784af48c04c41e6632cbaca620cba9
Lucas <lucas@Lucass-MacBook-Pro.local>
2023-01-25 14:01:02 -0300

    animate: appointments total

--- frontend/src/components/Appointments/index.tsx
+++ frontend/src/components/Appointments/index.tsx
@@ -1,11 +1,15 @@
 import { AppointmentProps } from "../../types/appointment";
+import { AnimateNumber } from "../../utils/animateNumber";
 import { AppointmentCard } from "./AppointmentCard";
 
 export function Appointments({ appointments }: { appointments: AppointmentProps[] }) {
   return (
     <div className="flex flex-col space-y-5 w-full max-w-[600px] px-5 items-center h-full">
       <p className="text-lg text-gray-800 text-left w-full">
-        {appointments?.length} consultas agendadas
+        <div className="flex space-x-2">
+          <AnimateNumber from={0} to={appointments?.length} />
+          <p>consultas agendadas</p>
+        </div>
       </p>
 
       <div className="w-full max-w-[600px] space-y-10 overflow-y-auto pb-40">
