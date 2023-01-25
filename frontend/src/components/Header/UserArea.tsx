85260774c7d44473cc956041a676171c97d17302
Lucas <lucas@Lucass-MacBook-Pro.local>
2023-01-25 12:03:26 -0300

    fix: user area responsivity

--- frontend/src/components/Header/UserArea.tsx
+++ frontend/src/components/Header/UserArea.tsx
@@ -4,8 +4,8 @@ export function UserArea() {
   const { logout, name } = useAuth();
 
   return (
-    <div className="flex gap-2 items-center w-full text-end justify-end">
-      <p className="w-full invisible md:visible">Olá, {name}</p>
+    <div className="flex gap-2 items-center w-full text-end justify-end space-x-5">
+      <p className="w-full hidden md:block">Olá, {name}</p>
       <div className="w-20">
         <button className="default w-20" onClick={() => logout()}>
           Sair
