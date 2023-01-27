f832e33de12528e18a2d77b0d3516da77933b29e
Lucas <lucas@Lucass-MacBook-Pro.local>
2023-01-27 15:34:59 -0300

    update: how dashboard story fetch data

--- frontend/src/stories/Dashboard.stories.tsx
+++ frontend/src/stories/Dashboard.stories.tsx
@@ -1,50 +1,18 @@
 import { Meta } from "@storybook/react";
-import { useAppointment } from "../context/appointment";
 import { useAuth } from "../context/auth";
 import { Dashboard } from "../pages/Dashboard";
 
 export default {
   title: "Dashboard",
   component: Dashboard,
-  decorators: [
-    (Story) => {
-      useAuth.setState({ name: "João da Silva" });
-
-      return <Story />;
-    },
-  ],
 } as Meta;
 
-export const Default = ({ token }) => {
-  const mockAppointments = [
-    {
-      id: 1,
-      patientId: 1,
-      date: "Fri Feb 05 2021 10:20:00 GMT-0300 (Brasilia Standard Time)",
-      patient: {
-        id: 1,
-        first_name: "Frodo",
-        last_name: "Baggins",
-        email: "frodo.baggins@mail.com",
-      },
-    },
-    {
-      id: 1,
-      patientId: 1,
-      date: "Fri Feb 05 2021 10:20:00 GMT-0300 (Brasilia Standard Time)",
-      patient: {
-        id: 1,
-        first_name: "Frodo",
-        last_name: "Baggins",
-        email: "frodo.baggins@mail.com",
-      },
-    },
-  ];
-  useAppointment.setState({ appointments: mockAppointments });
-
+export const Default = ({ email, password }) => {
+  useAuth().login({ email, password });
   return (
     <div className="bg-secondary">
-      <Dashboard mocked />;
+      <Dashboard />
     </div>
   );
 };
+Default.args = { email: "gandalf@mail.com", password: "123456" };
