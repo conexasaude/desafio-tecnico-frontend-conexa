56cdf68dbfea97c4d5c34256713238b1bbbf58a1
Lucas <lucas@Lucass-MacBook-Pro.local>
2023-01-29 21:41:07 -0300

    fix: axios auth token

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
-  headers: {
-    Authorization: `${localStorage.getItem("bearer")}`,
-  },
 });
 
 export { api };
